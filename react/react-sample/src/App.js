181ae6648f10d4d7584b0879c3496a549e0b8c0f
root <root@localhost.localdomain>
2023-04-04 01:50:03 +0000

    react側でAPI叩く

--- react/react-sample/src/App.js
+++ react/react-sample/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const url = "http://192.168.33.24:8000/api/test";
+
+  useEffect(()=>{
+    (async ()=>{
+      try{
+        const res = await axios.get(url);
+	console.log(res);
+        return;
+      }catch (e){
+        return e;
+      }
+    })();
+  },[]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"></div>
   );
 }
 
